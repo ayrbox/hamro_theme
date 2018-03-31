0180045f1b5d8571a7b1d32320b6265232b9b320
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-03-31 23:51:35 +0100

    FrontPage: Policy columns

---
+++ section-templates/section-policy.php
@@ -0,0 +1,40 @@
+<div class="container">
+  <div class="row">
+    <div class="col-4">
+      <div class="policy">
+        <div class="icon"><i class="fa fa-truck" aria-hidden="true">icon</i></div>
+        <div class="text">
+          <h3>Free Shipping</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-4">
+      <div class="policy">      
+        <div class="icon"><i class="fa fa-history" aria-hidden="true">icon</i></div>
+        <div class="text">
+          <h3>30 Days Money Back</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-4">
+
+      <div class="policy">
+        <div class="icon"><i class="fa fa-lock" aria-hidden="true">icon</i></div>
+        <div class="text">
+          <h3>Payment Secured</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</div>
