2d3c3fc206f54775c074164b268a2e67a8a8c2e6
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-04-08 19:54:07 +0100

    Add: Top and Footer menu navigation

--- inc/setup.php
+++ inc/setup.php
@@ -42,6 +42,8 @@ if ( ! function_exists( 'understrap_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'primary' => __( 'Primary Menu', 'understrap' ),
+			'top-menu' => __('Top Menu', 'understrap'),
+			'footer-menu' => __('Footer Menu', 'understrap')
 		) );
 
 		/*
