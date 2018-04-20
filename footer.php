e95aa430b291f664e27daa97c31cbadadf9b0a9d
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-04-20 14:04:19 +0100

    Add google analytics

--- footer.php
+++ footer.php
@@ -86,6 +86,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117933503-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-117933503-1');
+</script>
 
 </body>
 
