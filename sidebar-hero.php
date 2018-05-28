ca6afca9e63a617ca8c6823361a395cd9d45dc58
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-05-28 14:11:37 +0100

    UPDATE: Carousel css

--- sidebar-hero.php
+++ sidebar-hero.php
@@ -7,38 +7,29 @@
 
 ?>
 
-<?php if ( is_active_sidebar( 'hero' ) ) : ?>
+<?php if (is_active_sidebar('hero')) : ?>
+	
 
-	<!-- ******************* The Hero Widget Area ******************* -->
 
-		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-
-			<div class="carousel-inner" role="listbox">
-
-			<?php dynamic_sidebar( 'hero' ); ?>
-
-			</div>
-
-			 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-			    
-			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			    
-			    <span class="sr-only"><?php esc_html_e( 'Previous', 'understrap' ); ?></span>
-			 
-			 </a>
-			 
-			 <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-			    
-			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-			    
-			    <span class="sr-only"><?php esc_html_e( 'Next', 'understrap' ); ?></span>
-			  
-			</a>
-
-		</div><!-- .carousel -->
-
-<script>
-jQuery( ".carousel-item" ).first().addClass( "active" );
-</script>
-
+	<div id="mainCarousel" class="carousel slide page-carousel" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#mainCarousel" data-slide-to="1"></li>
+			<li data-target="#mainCarousel" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<?php dynamic_sidebar('hero'); ?>
+		</div>
+		<a class="carousel-control-prev" href="#mainCarousel" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#mainCarousel" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+	<script>
+		jQuery( ".carousel-item" ).first().addClass( "active" );
+	</script>
 <?php endif; ?>
