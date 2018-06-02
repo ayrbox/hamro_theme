0cfce688fe6686c53526473de107a281fb8a602c
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-06-02 14:00:30 +0100

    UPDATE: policy design

--- section-templates/section-policy.php
+++ section-templates/section-policy.php
@@ -1,46 +1,31 @@
-<div class="container">
-  <div class="row">
-    <div class="col-md-4">
-      <div class="policy">
-        <div class="icon"><i class="fa fa-truck" aria-hidden="true">icon</i></div>
-        <div class="text">
-          <h3>Free Delivery</h3>
-          <p>
-          Our standard delivery in mainland UK is free and takes up to 5 working days. 
-          We dispatach orders from Monday to Saturday. Generally items are dispatched 
+<div class="container mb-5 mt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-4 text-center">
+      <h3 class="mb-4">Free delivery</h3>
+      <p>
+          Our standard delivery in mainland UK is free and 
+          takes up to 5 working days. We dispatach orders from 
+          Monday to Saturday. Generally items are dispatched 
           next working day.
-          </p>
-        </div>
-      </div>
+      </p>
     </div>
-
-    <div class="col-md-4">
-      <div class="policy">      
-        <div class="icon"><i class="fa fa-history" aria-hidden="true">icon</i></div>
-        <div class="text">
-          <h3>28 Days Money Back</h3>
-          <p>
-          Most of our products can be returned in their original packaging within 28 
-          days if your are not happy with the product.  We make full refund or 
-          arrange an exchange for you.
-          </p>
-        </div>
-      </div>
+    <div class="col-md-4 text-center">
+      <h3 class="mb-4">28 Days Money Back</h3>      
+      <p>
+        Most of our products can be returned in their original 
+        packaging within 28 days if your are not happy with 
+        the product.  We make full refund or arrange an exchange 
+        for you.
+      </p>
     </div>
-
-
-    <div class="col-md-4">
-
-      <div class="policy">
-        <div class="icon"><i class="fa fa-lock" aria-hidden="true">icon</i></div>
-        <div class="text">
-          <h3>Payment Secured</h3>
-          <p>
-            We would like your payments to be secure thus only use <strong>PayPal</strong>. 
-            We do not see any card details as transaction is handled by <strong>PayPal</strong>.             
-          </p>
-        </div>
-      </div>
+    <div class="col-md-4 text-center">
+      <h3 class="mb-4">Secured Payment</h3>
+      <p>
+        We would like your payments to be secure thus only 
+        use <strong>PayPal</strong>. We do not see any card 
+        details as transaction is handled by <strong>PayPal</strong>.
+      </p>
     </div>
   </div>
 </div>
+
